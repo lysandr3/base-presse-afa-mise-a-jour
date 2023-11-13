6ac8b1ed9833f5f0811aa12f5ed772230ee73c06
lsand4 <90618221+lysandr3@users.noreply.github.com>
2023-11-13 14:37:20 +0000

    Adding authentification

--- Hello.py
+++ Hello.py
@@ -21,6 +21,10 @@ from io import StringIO
 from tempfile import NamedTemporaryFile
 import pandas as pd
 
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
 LOGGER = get_logger(__name__)
 
 
@@ -29,24 +33,39 @@ def run():
     page_title="BP MAJ",
     page_icon="",
   )
-  st.header("Base Presse AFA - Mise à jour :")
-  s3 = boto3.client('s3', aws_access_key_id=st.secrets['AWS_ACCESS_KEY_ID'], aws_secret_access_key=st.secrets['AWS_SECRET_ACCESS_KEY'])
 
-  uploaded_file = st.file_uploader("Veuillez importer la dernière copie numérique transmise", type=["pdf"])
-  if uploaded_file is not None:
-    temp_file = NamedTemporaryFile(delete=False)
-    temp_file.write(uploaded_file.read())
-    db = pdf_to_df(temp_file.name).applymap(str.strip)
-    st.write(db)
+  with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+    authenticator = stauth.Authenticate( config['credentials'], config['cookie']['name'], config['cookie']['key'], config['cookie']['expiry_days'], config['preauthorized'])
+  
+  name, authentication_status, username = authenticator.login('Login', 'main')
+  
+  if authentication_status:
+      authenticator.logout('Logout', 'main')
+      
+      st.header("Base Presse AFA - Mise à jour :")
+      s3 = boto3.client('s3', aws_access_key_id=st.secrets['AWS_ACCESS_KEY_ID'], aws_secret_access_key=st.secrets['AWS_SECRET_ACCESS_KEY'])
 
-    if st.button('Ajouter à la base de donnée ?'):
-      response = s3.get_object(Bucket='base-presse-afa', Key='bp.csv')
-      df = pd.read_csv(StringIO(response['Body'].read().decode('utf-8')))
-      csv_buffer = StringIO() 
-      # Change pd.DataFrame() by df when you'll stop to test it
-      pd.concat([db,pd.DataFrame()],ignore_index=True).to_csv(csv_buffer, index=False)
-      s3.put_object(Body=csv_buffer.getvalue(), Bucket='base-presse-afa', Key='bp.csv')
-      st.success("Base de donnée mise à jour !")
+      uploaded_file = st.file_uploader("Veuillez importer la dernière copie numérique transmise", type=["pdf"])
+      if uploaded_file is not None:
+        temp_file = NamedTemporaryFile(delete=False)
+        temp_file.write(uploaded_file.read())
+        db = pdf_to_df(temp_file.name).applymap(str.strip)
+        st.write(db)
+
+        if st.button('Ajouter à la base de donnée ?'):
+          response = s3.get_object(Bucket='base-presse-afa', Key='bp.csv')
+          df = pd.read_csv(StringIO(response['Body'].read().decode('utf-8')))
+          csv_buffer = StringIO() 
+          # Change pd.DataFrame() by df when you'll stop to test it
+          pd.concat([db,pd.DataFrame()],ignore_index=True).to_csv(csv_buffer, index=False)
+          s3.put_object(Body=csv_buffer.getvalue(), Bucket='base-presse-afa', Key='bp.csv')
+          st.success("Base de donnée mise à jour !")
+  
+  elif authentication_status == False:
+      st.error('Username/password is incorrect')
+  elif authentication_status == None:
+      st.warning('Please enter your username and password')
 
 if __name__ == "__main__":
     run()
